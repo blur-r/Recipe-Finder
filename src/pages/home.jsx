739ebc7cf7685b344a22273cba2e0d6456eca95a
Babalola Paul <pbabalola27@gmail.com>
2025-07-01 13:21:22 +0100

    Add HowItWorks section and styles to the home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Hero from "../sections/hero";
+import HowItWorks from "../sections/HowItWorks";
 
 function Home() {
     return (
         <>
             <Header />
             <Hero />
+            <HowItWorks />
         </>
     )
 }
---
+++ src/sections/HowItWorks.jsx
@@ -0,0 +1,51 @@
+
+
+function HowItWorks() {
+    return (
+        <>
+            <section className="how-it-works">
+                <h1 style={{ textAlign: "center" }}>How It Works</h1>
+                <div className="steps">
+                    <div className="step">
+                        <div className="step-icon">
+                            <i class="fas fa-plus"></i>
+                        </div>
+                        <h2>
+                            Add Your Ingredients
+                        </h2>
+                        <p>
+                            Simply type in the ingredients you have in your kitchen
+                        </p>
+                    </div>
+
+                    <div className="step">
+                        <div className="step-icon">
+                            <i class="fas fa-plus"></i>
+                        </div>
+                        <h2>
+                            Add Your Ingredients
+                        </h2>
+                        <p>
+                            Simply type in the ingredients you have in your kitchen
+                        </p>
+                    </div>
+
+                    <div className="step">
+                        <div className="step-icon">
+                            <i class="fas fa-plus"></i>
+                        </div>
+                        <h2>
+                            Add Your Ingredients
+                        </h2>
+                        <p>
+                            Simply type in the ingredients you have in your kitchen
+                        </p>
+                    </div>
+                </div>
+            </section>
+
+        </>
+    )
+}
+
+export default HowItWorks
