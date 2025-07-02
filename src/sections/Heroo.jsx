285f57037d5e2afb1e86ac2e0e19f68e83763f04
Babalola Paul <pbabalola27@gmail.com>
2025-07-02 11:58:56 +0100

    Fix punctuation in Hero component header and adjust header styles for better responsiveness

--- src/sections/Heroo.jsx
+++ src/sections/Heroo.jsx
@@ -4,7 +4,7 @@ function Hero() {
     return (
         <>
             <div className="hero" style={{ backgroundImage: `url(${heroImg})` }}>
-                <h1>Find Recipes with what you have</h1>
+                <h1>Find Recipes with what you have.</h1>
                 <p>Enter the Ingredient you have and get delicious Recipes Instantly. No more Wondering what to cook.</p>
                 <form action="">
                     <input type="search" placeholder="Enter your ingredients" />
